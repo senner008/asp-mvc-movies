d321b1d506b3cde9bf6ceebcab0c5362ef90d8f0
Niels Gregersen <nielshtg@gmail.com>
2019-10-23 01:22:35 +0200

    WIP

--- app/Controllers/MoviesController.cs
+++ app/Controllers/MoviesController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Attributes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
