dbb67c1dbc2e07d1df9959c25c455c8f14dea962
Niels Theil Gregersen <47080964+senner008@users.noreply.github.com>
2019-10-22 15:30:15 +0200

    WIP (#2)

--- tests/IntegrationTest.cs
+++ tests/IntegrationTest.cs
@@ -1,9 +1,11 @@
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using asp_mvc;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace tests
@@ -13,7 +15,16 @@ namespace tests
         public HttpClient _client;
         public IntegrationTest()
         {
-            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+                  string wanted_path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+            // TODO :  better way to get TodoApp path?
+            var parent = Directory.GetParent(wanted_path).Parent;
+
+             IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(parent.ToString())
+            .AddJsonFile("app\\appsettings.json")
+            .Build();
+       
+            var server = new TestServer(new WebHostBuilder().UseConfiguration(configuration).UseStartup<Startup>());
             _client = server.CreateClient();
         }
 
@@ -26,6 +37,27 @@ namespace tests
             var responseString = await response.Content.ReadAsStringAsync();
 
             Assert.Equal("Hello World", responseString);
+        }
+
+         [Fact]
+        public async Task Home_NoCondition_Success()        {
+            
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var headers = response.Content.Headers.ContentType.ToString();
+
+            Assert.Equal("text/html; charset=utf-8", headers);
+        }
+
+        [Fact]
+        public async Task Movies_NoCondition_Success()        {
+            
+            var response = await _client.GetAsync("Movies");
+            response.EnsureSuccessStatusCode();
+
+            var headers = response.Content.Headers.ContentType.ToString();
+
+            Assert.Equal("text/html; charset=utf-8", headers);
         }
     }
 }
