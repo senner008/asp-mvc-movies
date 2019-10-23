9047aa64449587ed032a08b4b31eddea2c6dfde2
Niels Theil Gregersen <47080964+senner008@users.noreply.github.com>
2019-10-23 18:11:06 +0200

    Feature/addrating (#9)
    * add prop
    
    * WIP

--- app/Models/SeedData.cs
+++ app/Models/SeedData.cs
@@ -26,6 +26,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
