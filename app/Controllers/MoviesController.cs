1bce508dd4e058f8521a4b18f25f94233445ef68
Niels Gregersen <nielshtg@gmail.com>
2019-10-27 23:09:42 +0100

    WIP

--- app/Controllers/MoviesController.cs
+++ app/Controllers/MoviesController.cs
@@ -1,37 +1,27 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.DataEncryption;
-using Microsoft.EntityFrameworkCore.DataEncryption.Providers;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MvcMovie.Data;
 using MvcMovie.Models;
 
 namespace asp_mvc.Controllers
 {
-    public class MoviesController : Controller
-    {
+    public class MoviesController : Controller {
         private readonly MvcMovieContext _context;
-        private readonly ILogger<MoviesController> _logger;
 
+        public ILogger<MoviesController> _logger { get; }
 
-        public MoviesController(MvcMovieContext context, ILogger<MoviesController> logger)
-        {
+        public MoviesController (MvcMovieContext context, ILogger<MoviesController> logger) {
             _context = context;
             _logger = logger;
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
-        {
-            var list = (await _context.Movie.ToListAsync());
-            return View(list);
+        public async Task<IActionResult> Index () {
+            return View (await _context.Movie.ToListAsync ());
         }
 
         // GET: Movies/Details/5
@@ -39,6 +29,7 @@ namespace asp_mvc.Controllers
         {
             if (id == null)
             {
+                Response.StatusCode = 404;
                 _logger.LogError($"Id not provided!");
                 return View("Error", "Intet id valgt!!");
             }
@@ -55,9 +46,8 @@ namespace asp_mvc.Controllers
         }
 
         // GET: Movies/Create
-        public IActionResult Create()
-        {
-            return View();
+        public IActionResult Create () {
+            return View ();
         }
 
         // POST: Movies/Create
@@ -65,87 +55,27 @@ namespace asp_mvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
-        {
-            if (ModelState.IsValid)
-            {
-                // movie.Title = _provider.Encrypt(movie.Title);
-                _context.Add(movie);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+        [Authorize (Roles = "Admin")]
+        public async Task<IActionResult> Create ([Bind ("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie) {
+            if (ModelState.IsValid) {
+                _context.Add (movie);
+                await _context.SaveChangesAsync ();
+                return RedirectToAction (nameof (Index));
             }
-            return View(movie);
+            return View (movie);
         }
 
         // GET: Movies/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
+        public async Task<IActionResult> Edit (int? id) {
+            if (id == null) {
+                return NotFound ();
             }
 
-            var movie = await _context.Movie.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
+            var movie = await _context.Movie.FindAsync (id);
+            if (movie == null) {
+                return NotFound ();
             }
-            return View(movie);
-        }
-
-        // GET: Movies/EditName/5
-        public async Task<IActionResult> EditName(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var movie = await _context.Movie.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-            return View(movie);
-        }
-
-         // POST: Movies/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> EditName(int id, [Bind("Id, Title")] Movie movie)
-        {
-            if (id != movie.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    var _movie = await _context.Movie.FindAsync(id);
-                    _movie.Title = movie.Title;
-                    _context.Update(_movie);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieExists(movie.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(movie);
+            return View (movie);
         }
 
         // POST: Movies/Edit/5
@@ -153,92 +83,69 @@ namespace asp_mvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price, Rating")] Movie movie)
-        {
-            if (id != movie.Id)
-            {
-                return NotFound();
+        [Authorize (Roles = "Admin")]
+        public async Task<IActionResult> Edit (int id, [Bind ("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie) {
+            if (id != movie.Id) {
+                return NotFound ();
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(movie);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieExists(movie.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
+            if (ModelState.IsValid) {
+                try {
+                    _context.Update (movie);
+                    await _context.SaveChangesAsync ();
+                } catch (DbUpdateConcurrencyException) {
+                    if (!MovieExists (movie.Id)) {
+                        return NotFound ();
+                    } else {
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction (nameof (Index));
             }
-            return View(movie);
+            return View (movie);
         }
 
         // GET: Movies/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                 _logger.LogError($"Please provide id for the movie to delete");
-                Response.StatusCode = 404;
-                return View("Error", "Film med id ikke angivet!");
+        public async Task<IActionResult> Delete (int? id) {
+            if (id == null) {
+                return NotFound ();
             }
 
             var movie = await _context.Movie
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (movie == null)
-            {
-                _logger.LogError($"The movie does not exist!");
-                Response.StatusCode = 404;
-                return View("Error", "Film med id findes ikke!");
+                .FirstOrDefaultAsync (m => m.Id == id);
+            if (movie == null) {
+                return NotFound ();
             }
 
-            return View(movie);
+            return View (movie);
         }
 
         // POST: Movies/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName ("Delete")]
         [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var movie = await _context.Movie.FindAsync(id);
-            try 
-            {
-                _context.Movie.Remove(movie);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MovieExists(movie.Id))
-                {
-                    _logger.LogError($"The movie does not exist! Possibly deleted by another admin user");
+        [Authorize (Roles = "Admin")]
+        public async Task<IActionResult> DeleteConfirmed (int id) {
+
+            var movie = await _context.Movie.FindAsync (id);
+
+            try {
+                _context.Movie.Remove (movie);
+                await _context.SaveChangesAsync ();
+
+            } catch (DbUpdateConcurrencyException) {
+
+                if (!MovieExists (movie.Id)) {
+                    _logger.LogError ($"The movie does not exist! Possibly deleted by another admin user");
                     Response.StatusCode = 404;
-                    return View("Error", "Film med id findes ikke! Muligvis slettet af anden admin bruger");
-                }
-                else
-                {
+                    return View ("Error", "Film med id findes ikke! Muligvis slettet af anden admin bruger");
+                } else {
                     throw;
                 }
             }
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction (nameof (Index));
         }
-
-        private bool MovieExists(int id)
-        {
-            return _context.Movie.Any(e => e.Id == id);
+        private bool MovieExists (int id) {
+            return _context.Movie.Any (e => e.Id == id);
         }
-
-
     }
 }
--- app/Migrations/20191027202803_movietoreviews.Designer.cs
+++ app/Migrations/20191027202803_movietoreviews.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
 namespace asp_mvc.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20191027202803_movietoreviews")]
+    partial class movietoreviews
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
