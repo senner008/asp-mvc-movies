cbdd4f939f4a91bb02f4a3d0021be9f8966a3bc3
Niels Gregersen <nielshtg@gmail.com>
2019-10-29 18:36:27 +0100

    add date to review

--- app/Migrations/20191029172433_reviewdate.Designer.cs
+++ app/Migrations/20191029172433_reviewdate.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
 namespace asp_mvc.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20191029172433_reviewdate")]
+    partial class reviewdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
