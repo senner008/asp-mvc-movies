74ffc74b612f2f25dbb187701b39f3fa742d9a16
Niels Gregersen <nielshtg@gmail.com>
2019-10-30 17:06:23 +0100

    WIP

--- app/Controllers/MoviesController.cs
+++ app/Controllers/MoviesController.cs
@@ -92,7 +92,7 @@ namespace asp_mvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-         [Authorize (Roles = "Admin")]
+        [Authorize (Roles = "Admin")]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
@@ -144,7 +144,7 @@ namespace asp_mvc.Controllers
         // POST: Movies/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-         [Authorize (Roles = "Admin")]
+        [Authorize (Roles = "Admin")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
              var movie = await _context.Movie.FindAsync(id);
